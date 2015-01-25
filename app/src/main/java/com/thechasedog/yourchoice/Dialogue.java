1d024485cf6f54a1cfd441ece53dbb2d20a55b18
kwirkykat <katharina.theresa.keim@gmail.com>
2015-01-24 19:06:15 -0800

    adding dialogues

---
+++ app/src/main/java/com/thechasedog/yourchoice/Dialogue.java
@@ -0,0 +1,13 @@
+package com.thechasedog.yourchoice;
+
+import java.util.List;
+
+/**
+ * Created by katha_000 on 1/24/2015.
+ */
+public class Dialogue {
+    public String title;
+    public List<String> requirements;
+    public String text;
+    public String id;
+}
