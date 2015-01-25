97b78ffea7957f0c96bf65ace6882cdcc3d3e66c
kwirkykat <katharina.theresa.keim@gmail.com>
2015-01-24 20:07:13 -0800

    adding option class

---
+++ app/src/main/java/com/thechasedog/yourchoice/Option.java
@@ -0,0 +1,15 @@
+package com.thechasedog.yourchoice;
+
+import java.util.List;
+
+/**
+ * Created by katha_000 on 1/24/2015.
+ */
+public class Option {
+    public String id;
+    public List<String> requirements;
+    public List<String> modifiers;
+    public String text;
+
+    public Option() {}
+}
